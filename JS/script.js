3894594bfdf51b1555ca2d458a76e278f085877a
Potahtohh <ayush.shrestha2580@gmail.com>
2023-04-14 12:35:25 +0545

    Static e-commerce website

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+
+
+function popup() {
+    var msg = window.confirm("Do you want to buy this product?");
+
+    if (msg == true) {
+        alert("Please contact us through Contact Us form\n  or\nEmail: mobilenation@gmail.com\nPhone Number: 01-5972878");
+    }
+
+    else{
+        alert("Be sure to check out other products too.")
+    }
+}
+
+
+
